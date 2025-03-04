d572c5a806b85a90445ec61cec086f527ba25eb8
dheljohn <153157935+dheljohn@users.noreply.github.com>
2025-03-04 21:27:39 +0800

    fix levels not being seen

--- lib/about.dart
+++ lib/about.dart
@@ -29,9 +29,8 @@ class AboutPage extends StatelessWidget {
               unselectedLabelColor: Color.fromARGB(200, 255, 255, 255),
               indicatorColor: Colors.white,
               tabs: [
-                Tab(text: "Soil Moisture Levels"),
+                Tab(text: "Soil Moisture \n     Levels"),
                 Tab(text: "Application"),
-
                 Tab(text: "Developers"),
               ],
             ),
@@ -42,7 +41,6 @@ class AboutPage extends StatelessWidget {
             SoilMoistureInfo(),
             const AboutTab(),
             const DeveloperPage(),
-
           ],
         ),
       ),
@@ -64,91 +62,91 @@ class AboutTab extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // App Image
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
-                  'assets/app.jpg', 
-                  width: 250,
-                  height: 150,
-                  fit: BoxFit.cover,
+              // App Image
+              Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/app.jpg',
+                    width: 250,
+                    height: 150,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // About Section
-            Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: const Color.fromARGB(255, 42, 83, 39),
-                  width: 2,
+              // About Section
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 42, 83, 39),
+                    width: 2,
+                  ),
                 ),
-              ),
-              padding: const EdgeInsets.all(15),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'SOMO',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 125, 171, 124),
+                padding: const EdgeInsets.all(15),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'SOMO',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 125, 171, 124),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'SOMO (Soil Moisture Monitoring) is an application designed to help you keep track of soil moisture conditions in real-time. Whether you are a farmer, gardener, or plant enthusiast, SOMO provides data on soil moisture, temperature, and humidity, ensuring your plants get the right amount of water they need to thrive.',
-                    style: TextStyle(fontSize: 16, color: Colors.black87),
-                    textAlign: TextAlign.justify,
-                  ),
-                ],
+                    SizedBox(height: 10),
+                    Text(
+                      'SOMO (Soil Moisture Monitoring) is an application designed to help you keep track of soil moisture conditions in real-time. Whether you are a farmer, gardener, or plant enthusiast, SOMO provides data on soil moisture, temperature, and humidity, ensuring your plants get the right amount of water they need to thrive.',
+                      style: TextStyle(fontSize: 16, color: Colors.black87),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
               ),
-            ),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // Key Features Section
-            const Text(
-              "Key Features",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 42, 83, 39),
+              // Key Features Section
+              const Text(
+                "Key Features",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 42, 83, 39),
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
+              const SizedBox(height: 10),
 
-            // Features List
-            const FeatureItem(
-              title: "📊 Dashboard",
-              description:
-                  "Monitor real-time soil moisture levels, temperature, and humidity all in one place.",
-            ),
-            const FeatureItem(
-              title: "💬 Helper Message",
-              description:
-                  "Receive guidance and tips on maintaining optimal soil conditions for plant growth.",
-            ),
-            const FeatureItem(
-              title: "🔍 Recommendations",
-              description:
-                  "Get AI-based watering suggestions based on soil moisture data and weather conditions.",
-            ),
-            const FeatureItem(
-              title: "🔔 Notifications",
-              description:
-                  "Stay updated with alerts when soil conditions require attention.",
-            ),
-          ],
+              // Features List
+              const FeatureItem(
+                title: "📊 Dashboard",
+                description:
+                    "Monitor real-time soil moisture levels, temperature, and humidity all in one place.",
+              ),
+              const FeatureItem(
+                title: "💬 Helper Message",
+                description:
+                    "Receive guidance and tips on maintaining optimal soil conditions for plant growth.",
+              ),
+              const FeatureItem(
+                title: "🔍 Recommendations",
+                description:
+                    "Get AI-based watering suggestions based on soil moisture data and weather conditions.",
+              ),
+              const FeatureItem(
+                title: "🔔 Notifications",
+                description:
+                    "Stay updated with alerts when soil conditions require attention.",
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
@@ -159,7 +157,8 @@ class FeatureItem extends StatelessWidget {
   final String title;
   final String description;
 
-  const FeatureItem({super.key, required this.title, required this.description});
+  const FeatureItem(
+      {super.key, required this.title, required this.description});
 
   @override
   Widget build(BuildContext context) {
